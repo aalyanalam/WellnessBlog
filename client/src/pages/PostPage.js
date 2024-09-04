0002baf514c4d3eff8f9918f37b6c31dd17a90b6
aalyanalam <122890737+aalyanalam@users.noreply.github.com>
2024-09-04 19:59:57 -0400

    Update PostPage.js

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -7,7 +7,7 @@ export default function PostPage() {
     const [postInfo,setPostInfo] = useState(null);
     const {id} = useParams();
     useEffect(() => {
-       fetch(`http://ec2-3-138-135-207.us-east-2.compute.amazonaws.com:4000/post/${id}`)
+       fetch(`IPV4_DNS/post/${id}`)
        .then(response => {
         response.json().then(postInfo => {
             setPostInfo(postInfo);
@@ -22,7 +22,7 @@ export default function PostPage() {
             <time>{format(new Date(postInfo.createdAt), 'MMM d, yyyy HH:mm')}</time>
             <div className="author">by @{postInfo.author.username}</div>
             <div className="image">
-            <img src={`http://ec2-52-15-155-181.us-east-2.compute.amazonaws.com:4000/${postInfo.cover}`} alt=""/>
+            <img src={`IPV4_DNS/${postInfo.cover}`} alt=""/>
             </div>
             <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}}></div>
         </div>
